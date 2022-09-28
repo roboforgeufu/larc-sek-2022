210de8f9ea06987a26ffd7f61c5ec1030760c661
Otávio Leite <otavio.leite.ufu@gmail.com>
2022-09-28 19:58:47 -0300

    adiciona o alinhador de parede ao código da parada temporizada

--- drafts/wall_follower_alts/wall_aligner.py
+++ drafts/wall_follower_alts/wall_aligner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+
 import time
 
 from pybricks.ev3devices import InfraredSensor, Motor
@@ -87,86 +88,6 @@ def wall_aligner(dc_speed: int = 30, max_angle: int = 300):
     hold_all(motor_r, motor_l)
 
 
-TARGET_DISTANCE = 20
-STOP_TIME_MS = 100
-ANGLE_RESET_TIME = STOP_TIME_MS * 8
-
-TARGET_DIFF_NEAR = -2
-TARGET_DIFF_FAR = 2
-
-DC_SPEED = 30
-
-MOTOR_DIFF_CEIL = 350
-
-
-stop_left_flag = False
-stop_right_flag = False
-right_angle_monitor = motor_r.angle()
-left_angle_monitor = motor_l.angle()
-side_counter = 0
-
 while True:
-    if side_counter >= 8:
-        # alinha na parede caso tenha tentado muitas vezes ir pra direita
-        ev3.speaker.beep(frequency=100)
-        ev3.light.on(Color.GREEN)
-
-        wall_aligner()
-
-        ev3.light.off()
-        ev3.speaker.beep()
-        side_counter = 0
-
-    target_diff = infra_side.distance() - TARGET_DISTANCE
-    # target_diff > 0: muito longe
-    # target_diff < 0: muito perto
-
-    acc_angle_left = motor_l.angle() - left_angle_monitor
-    acc_angle_right = motor_r.angle() - right_angle_monitor
-    motors_diff = acc_angle_left - acc_angle_right
-
-    if target_diff > TARGET_DIFF_FAR and not stop_right_flag:
-        # muito longe
-        # para o motor da direita por um tempinho
-        stop_right_flag = True
-        cronometer.reset()
-        ev3.light.on(Color.ORANGE)
-        side_counter += 1
-
-    elif target_diff < TARGET_DIFF_NEAR and not stop_left_flag:
-        # muito perto
-        # para o motor da esquerda por um tempinho
-        stop_left_flag = True
-        cronometer.reset()
-        ev3.light.on(Color.RED)
-        side_counter -= 1
-
-    # Reset das flags de acordo com o tempo
-    if any([stop_right_flag, stop_left_flag]) and cronometer.time() > STOP_TIME_MS:
-        stop_left_flag = stop_right_flag = False
-        ev3.light.off()
-
-    if cronometer.time() > ANGLE_RESET_TIME:
-        right_angle_monitor = motor_r.angle()
-        left_angle_monitor = motor_r.angle()
-        if side_counter < 0:
-            side_counter = 0
-
-    # Controle de velocidade (a partir das flags)
-    speed_left = speed_right = DC_SPEED
-    if stop_left_flag:
-        speed_left = 0
-    elif stop_right_flag:
-        speed_right = 0
-
-    # Atribuição de velocidade
-    motor_l.dc(speed_left)
-    motor_r.dc(speed_right)
-
-    # -- LOGGING ---
-    # ev3_print(target_diff, cronometer.time(), stop_left_flag, stop_right_flag)
-    ev3_print(side_counter)
-
-
-motor_l.hold()
-motor_r.hold()
+    wall_aligner()
+    time.sleep(1)
