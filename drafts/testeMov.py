e8270392a58a944d417616e8b89fdfbfc9b2f292
Otávio Leite <atoivat.o@gmail.com>
2022-10-03 15:49:09 -0300

    reestruturação de projeto + testes round_mapping + testes localização katara

--- drafts/testeMov.py
+++ drafts/testeMov.py
@@ -1,55 +1,64 @@
 #!/usr/bin/env pybricks-micropython
+from pybricks.ev3devices import ColorSensor, Motor
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.parameters import Port, Color, Stop
+from pybricks.parameters import Color, Port, Stop
 from pybricks.tools import StopWatch, wait
+
 # Initialize the EV3 brick.
 ev3 = EV3Brick()
 # Initialize.
 motorB = Motor(Port.B)
 motorC = Motor(Port.C)
-motorA = Motor(Port.A)
+# motorA = Motor(Port.A)
 cronometro = StopWatch()
 
-def curva(angulo): #angulo positivo: direita, negativo: esquerda
+
+def curva(angulo):  # angulo positivo: direita, negativo: esquerda
     motorB.reset_angle(0)
     motorC.reset_angle(0)
     Kp = 4
     Ki = 0.5
     Kd = 10
-    
+
     t = 0
     integ = 0
     erro = 0
-    while(motorB.angle()!=angulo):
-        media = (motorB.angle() - motorC.angle())/2
+    while motorB.angle() != angulo:
+        media = (motorB.angle() - motorC.angle()) / 2
         erro0 = erro
         erro = angulo - media
 
-        prop = erro*Kp
-        if(-3<erro<3): integ = integ+(erro*Ki)
+        prop = erro * Kp
+        if -3 < erro < 3:
+            integ = integ + (erro * Ki)
         t0 = t
         t = cronometro.time()
         tempoDecor = t - t0
-        if(tempoDecor<1): tempoDecor = 1
-        deriv = ((erro - erro0)*Kd)/tempoDecor
+        # if tempoDecor < 1:
+        #     tempoDecor = 1
+        deriv = ((erro - erro0) * Kd) / tempoDecor
 
-        correcao = prop+integ+deriv
+        correcao = prop + integ + deriv
         vel = 5 + correcao
-        if(vel<0):
-            if(vel>-5): vel = -5
+        if vel < 0:
+            if vel > -5:
+                vel = -5
         else:
-            if(vel<5): vel = 5
+            if vel < 5:
+                vel = 5
         motorC.run(-vel)
         motorB.run(vel)
 
     motorC.hold()
     motorB.hold()
 
-def anda_reto_graus(velBase,graus,stop): #para dar ré os dois valores devem ser negativos
-    Kp = 3                               #stop 1: hold 2: brake 3: coast
+
+def anda_reto_graus(
+    velBase, graus, stop
+):  # para dar ré os dois valores devem ser negativos
+    Kp = 3  # stop 1: hold 2: brake 3: coast
     Ki = 0.02
-    Kd = 3 
+    Kd = 3
 
     t = 0
     integ = 0
@@ -58,183 +67,197 @@ def anda_reto_graus(velBase,graus,stop): #para dar ré os dois valores devem ser
     motorB.reset_angle(0)
     motorC.reset_angle(0)
     cronometro.reset()
-    if(graus<0):
-        while(media>graus):
-            media = (motorB.angle() + motorC.angle())/2
+    if graus < 0:
+        while media > graus:
+            media = (motorB.angle() + motorC.angle()) / 2
             erro0 = erro
             erro = motorC.angle() - motorB.angle()
 
-            prop = erro*Kp 
-            if(-3<erro<3): integ = integ+(erro*Ki)
+            prop = erro * Kp
+            if -3 < erro < 3:
+                integ = integ + (erro * Ki)
             t0 = t
             t = cronometro.time()
             tempoDecor = t - t0
-            if(tempoDecor<1): tempoDecor = 1
-            deriv = ((erro - erro0)*Kd)/tempoDecor
+            if tempoDecor < 1:
+                tempoDecor = 1
+            deriv = ((erro - erro0) * Kd) / tempoDecor
 
-            correcao = prop+integ+deriv
-            motorC.run(velBase-correcao)
-            motorB.run(velBase+correcao)
+            correcao = prop + integ + deriv
+            motorC.run(velBase - correcao)
+            motorB.run(velBase + correcao)
     else:
-        while(media<graus):
-            media = (motorB.angle() + motorC.angle())/2
+        while media < graus:
+            media = (motorB.angle() + motorC.angle()) / 2
             erro0 = erro
             erro = motorC.angle() - motorB.angle()
 
-            prop = erro*Kp 
-            if(-3<erro<3): integ = integ+(erro*Ki)
+            prop = erro * Kp
+            if -3 < erro < 3:
+                integ = integ + (erro * Ki)
             t0 = t
             t = cronometro.time()
             tempoDecor = t - t0
-            if(tempoDecor<1): tempoDecor = 1
-            deriv = ((erro - erro0)*Kd)/tempoDecor
+            if tempoDecor < 1:
+                tempoDecor = 1
+            deriv = ((erro - erro0) * Kd) / tempoDecor
 
-            correcao = prop+integ+deriv
-            motorC.run(velBase-correcao)
-            motorB.run(velBase+correcao)
-    if(stop==1):
+            correcao = prop + integ + deriv
+            motorC.run(velBase - correcao)
+            motorB.run(velBase + correcao)
+    if stop == 1:
         motorC.hold()
         motorB.hold()
-    if(stop==2):
+    if stop == 2:
         motorC.brake()
         motorB.brake()
-    if(stop==3):
+    if stop == 3:
         return
 
+
 def walk(aFuncao=0, bFuncao=0, cFuncao=0, graus=0, intervOscilacao=0, insideReset=True):
-        """Anda com o robo."""
-        # TODO: Deixar velocidade constante.
-        print("Walking...")
-        if insideReset:
-            motorC.reset_angle(0)
-            motorB.reset_angle(0)
-        velocDir = 0
-        velocEsq = 0
-        while True:
-            # print(motorC.angle(), motorB.angle())
-            # Funcao que descreve a velocidade em funcao da porcentagem do deslocamento ja realizado
-            percDeslocado = (motorC.angle() / graus) * 100
-            velocEsq = (aFuncao * (percDeslocado ** 2) + bFuncao * percDeslocado + cFuncao) * 10
-            velocDir = velocEsq
+    """Anda com o robo."""
+    # TODO: Deixar velocidade constante.
+    print("Walking...")
+    if insideReset:
+        motorC.reset_angle(0)
+        motorB.reset_angle(0)
+    velocDir = 0
+    velocEsq = 0
+    while True:
+        # print(motorC.angle(), motorB.angle())
+        # Funcao que descreve a velocidade em funcao da porcentagem do deslocamento ja realizado
+        percDeslocado = (motorC.angle() / graus) * 100
+        velocEsq = (
+            aFuncao * (percDeslocado**2) + bFuncao * percDeslocado + cFuncao
+        ) * 10
+        velocDir = velocEsq
 
-            # Teto
-            if velocEsq > 900:
-                velocEsq = 900
-            if velocDir > 900:
-                velocDir = 900
+        # Teto
+        if velocEsq > 900:
+            velocEsq = 900
+        if velocDir > 900:
+            velocDir = 900
 
-            diferenca_EsqDir = abs(motorC.angle()) - abs(motorB.angle())
-            if abs(diferenca_EsqDir) > 3:
-                if diferenca_EsqDir > 0:
-                    # motorC andou mais, joga mais velocidade no motorB
-                    velocEsq = velocEsq * (1 - (intervOscilacao / 100))
-                    velocDir = velocDir * (1 + (intervOscilacao / 100))
-                else:
-                    # motorB andou mais, joga mais velocidade no motorC
-                    velocEsq = velocEsq * (1 + (intervOscilacao / 100))
-                    velocDir = velocDir * (1 - (intervOscilacao / 100))
-            motorC.run(velocEsq)
-            motorB.run(velocDir)
-            if (abs(velocEsq) < 50 and abs(velocDir) < 50) or percDeslocado >= 99:
-                break
+        diferenca_EsqDir = abs(motorC.angle()) - abs(motorB.angle())
+        if abs(diferenca_EsqDir) > 3:
+            if diferenca_EsqDir > 0:
+                # motorC andou mais, joga mais velocidade no motorB
+                velocEsq = velocEsq * (1 - (intervOscilacao / 100))
+                velocDir = velocDir * (1 + (intervOscilacao / 100))
+            else:
+                # motorB andou mais, joga mais velocidade no motorC
+                velocEsq = velocEsq * (1 + (intervOscilacao / 100))
+                velocDir = velocDir * (1 - (intervOscilacao / 100))
+        motorC.run(velocEsq)
+        motorB.run(velocDir)
+        if (abs(velocEsq) < 50 and abs(velocDir) < 50) or percDeslocado >= 99:
+            break
+
 
 def turn(aFuncao, bFuncao, cFuncao, grausCurva, fix=True):
-        print("Turning...")
-        self.lmotor.reset_angle(0)
-        self.rmotor.reset_angle(0)
+    print("Turning...")
+    self.lmotor.reset_angle(0)
+    self.rmotor.reset_angle(0)
 
-        # P/ calibrar
-        K = const.K
-        grausMotor = int(K * grausCurva / 90)
+    # P/ calibrar
+    K = const.K
+    grausMotor = int(K * grausCurva / 90)
 
-        mediaPercorrida = 0
-        while mediaPercorrida < 99:
-            # Calculo da velocidade de ambos os motores em funcao da media do deslocamento
-            # percorrido por cada um
-            percPercorridoEsq = (abs(self.lmotor.angle()) / grausMotor) * 100
-            percPercorridoDir = (abs(self.rmotor.angle()) / grausMotor) * 100
-            mediaPercorrida = (percPercorridoDir + percPercorridoEsq) / 2.0
+    mediaPercorrida = 0
+    while mediaPercorrida < 99:
+        # Calculo da velocidade de ambos os motores em funcao da media do deslocamento
+        # percorrido por cada um
+        percPercorridoEsq = (abs(self.lmotor.angle()) / grausMotor) * 100
+        percPercorridoDir = (abs(self.rmotor.angle()) / grausMotor) * 100
+        mediaPercorrida = (percPercorridoDir + percPercorridoEsq) / 2.0
 
-            velocidade = (
-                aFuncao * (mediaPercorrida ** 2) + bFuncao * mediaPercorrida + cFuncao
-            ) * 10
+        velocidade = (
+            aFuncao * (mediaPercorrida**2) + bFuncao * mediaPercorrida + cFuncao
+        ) * 10
 
-            self.lmotor.run(velocidade)
-            self.rmotor.run(-velocidade)
-        self.lmotor.stop()
-        self.rmotor.stop()
+        self.lmotor.run(velocidade)
+        self.rmotor.run(-velocidade)
+    self.lmotor.stop()
+    self.rmotor.stop()
 
-        if fix:
-            while True:
-                # Para o motor Esquerdo:
-                difEsq = abs(self.lmotor.angle()) - grausMotor
-                #print("DifEsq >>",difEsq, ">>", self.lmotor.angle(), grausMotor)
-                if abs(difEsq) > 3:
-                    # A diferenca eh consideravel
-                    if difEsq > 0:
-                        # Andou mais do que devia
-                        sinalEsq = -1 *(
-                            self.lmotor.angle() / abs(self.lmotor.angle())
-                        )  # Deve se movimentar no sentido contrario ao mov anterior
-                    else:
-                        # Andou menos do que devia
-                        sinalEsq = self.lmotor.angle() / abs(
-                            self.lmotor.angle()
-                        )  # Deve se movimentar no mesmo sentido do movimento anterior
-                    velocEsq = sinalEsq * (2 * abs(difEsq) + 50)
+    if fix:
+        while True:
+            # Para o motor Esquerdo:
+            difEsq = abs(self.lmotor.angle()) - grausMotor
+            # print("DifEsq >>",difEsq, ">>", self.lmotor.angle(), grausMotor)
+            if abs(difEsq) > 3:
+                # A diferenca eh consideravel
+                if difEsq > 0:
+                    # Andou mais do que devia
+                    sinalEsq = -1 * (
+                        self.lmotor.angle() / abs(self.lmotor.angle())
+                    )  # Deve se movimentar no sentido contrario ao mov anterior
                 else:
-                    # A diferenca eh irrelevante
-                    velocEsq = 0
+                    # Andou menos do que devia
+                    sinalEsq = self.lmotor.angle() / abs(
+                        self.lmotor.angle()
+                    )  # Deve se movimentar no mesmo sentido do movimento anterior
+                velocEsq = sinalEsq * (2 * abs(difEsq) + 50)
+            else:
+                # A diferenca eh irrelevante
+                velocEsq = 0
 
-                # Para o motor Direito:
-                difDir = abs(self.rmotor.angle()) - grausMotor
-                #print("DifDir >>", difDir, ">>", self.rmotor.angle(), grausMotor)
-                if abs(difDir) > 3:
-                    # A diferenca eh consideravel
-                    if difDir > 0:
-                        # Andou mais do que devia
-                        sinalDir = -1 * (
-                            self.rmotor.angle() / abs(self.rmotor.angle())
-                        )  # Deve se movimentar no sentido contrario ao mov anterior
-                    else:
-                        # Andou menos do que devia
-                        sinalDir = self.rmotor.angle() / abs(
-                            self.rmotor.angle()
-                        )  # Deve se movimentar no mesmo sentido do movimento anterior
-                    velocDir = sinalDir * (2 * abs(difDir) + 50)
+            # Para o motor Direito:
+            difDir = abs(self.rmotor.angle()) - grausMotor
+            # print("DifDir >>", difDir, ">>", self.rmotor.angle(), grausMotor)
+            if abs(difDir) > 3:
+                # A diferenca eh consideravel
+                if difDir > 0:
+                    # Andou mais do que devia
+                    sinalDir = -1 * (
+                        self.rmotor.angle() / abs(self.rmotor.angle())
+                    )  # Deve se movimentar no sentido contrario ao mov anterior
                 else:
-                    # A diferenca eh irrelevante
-                    velocDir = 0
+                    # Andou menos do que devia
+                    sinalDir = self.rmotor.angle() / abs(
+                        self.rmotor.angle()
+                    )  # Deve se movimentar no mesmo sentido do movimento anterior
+                velocDir = sinalDir * (2 * abs(difDir) + 50)
+            else:
+                # A diferenca eh irrelevante
+                velocDir = 0
 
-                # Por fim, manda a velocidade calculada para os motores
-                self.lmotor.run(velocEsq)
-                self.rmotor.run(velocDir)
+            # Por fim, manda a velocidade calculada para os motores
+            self.lmotor.run(velocEsq)
+            self.rmotor.run(velocDir)
 
-                # Se qualquer dos motores estiver fora do intervalo
-                # seguro, entao ele ainda nao comeca a contar
-                if (difDir not in range(-3, 4)) or (difEsq not in range(-3, 4)):
-                    self.stopwatch.reset()  # Nao comecar a contar <=> resetar constantemente o self.stopwatch
+            # Se qualquer dos motores estiver fora do intervalo
+            # seguro, entao ele ainda nao comeca a contar
+            if (difDir not in range(-3, 4)) or (difEsq not in range(-3, 4)):
+                self.stopwatch.reset()  # Nao comecar a contar <=> resetar constantemente o self.stopwatch
 
-                #print("Tempo:", self.stopwatch.time(), "\ Vel:", velocDir, velocEsq)
-                #print(difDir, difEsq)
-                # Caso passem 400 ms sem resetar o self.stopwatch, ou seja, 300 ms com ambos os motores na zona segura
-                if self.stopwatch.time() > 100:
-                    print("E:", self.lmotor.angle(), "D:", self.rmotor.angle())
-                    print(velocEsq, velocDir)
-                    print()
-                    break
+            # print("Tempo:", self.stopwatch.time(), "\ Vel:", velocDir, velocEsq)
+            # print(difDir, difEsq)
+            # Caso passem 400 ms sem resetar o self.stopwatch, ou seja, 300 ms com ambos os motores na zona segura
+            if self.stopwatch.time() > 100:
+                print("E:", self.lmotor.angle(), "D:", self.rmotor.angle())
+                print(velocEsq, velocDir)
+                print()
+                break
 
-def sobe_garra(): 
+
+def sobe_garra():
     cronometro.reset()
-    while(cronometro.time()<400): motorA.dc(100)
+    while cronometro.time() < 400:
+        motorA.dc(100)
     motorA.hold()
     wait(500)
 
-def desce_garra(): 
+
+def desce_garra():
     cronometro.reset()
-    while(cronometro.time()<300): motorA.dc(-100)
+    while cronometro.time() < 300:
+        motorA.dc(-100)
     motorA.hold()
     wait(500)
 
-#anda_reto_graus(300,2000,2)
-walk(aFuncao = -0.01, bFuncao = 1, cFuncao=40, graus=2000)
+
+# anda_reto_graus(300,2000,2)
+# walk(aFuncao=-0.01, bFuncao=1, cFuncao=40, graus=2000)
+curva(-220)
---
+++ drafts/testelocal.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.ev3devices import ColorSensor, Motor
+from pybricks.hubs import EV3Brick
+from pybricks.parameters import Color, Port, Stop
+from pybricks.tools import StopWatch, wait
+
+# Initialize the EV3 brick.
+ev3 = EV3Brick()
+# Initialize.
+motorB = Motor(Port.B)
+motorC = Motor(Port.C)
+# motorA = Motor(Port.A)
+cronometro = StopWatch()
+# sensorc1 = ColorSensor(Port.S1)
+# sensorc2 = ColorSensor(Port.S2)
+
+
+def buraco(vel):
+    flag = 0
+    tempo = (3200 - 5 * vel) / 2
+    reflex1 = sensorc1.reflection()
+    reflex2 = sensorc2.reflection()
+    while reflex1 > 60 and reflex2 > 60:
+        motorC.run(vel)
+        motorB.run(vel)
+    motorC.brake()
+    motorB.brake()
+
+    if reflex1 <= 60:
+        cronometro.reset()
+        while cronometro.time() < tempo:
+            velT = vel - cronometro.time()
+            if velT < vel / 3:
+                velT = vel / 3
+            motorB.run(velT)
+            motorC.run(velT)
+        motorC.hold()
+        motorB.hold()
+        wait(200)
+        if reflex1 < 10:
+            ev3.speaker.beep()
+            flag = 1
+        wait(200)
+        if sensorc1.color() != None and sensorc1.color() != Color.WHITE:
+            print(sensorc1.color())
+            ev3.speaker.beep(1000)
+            flag = 2
+        cronometro.reset()
+        while cronometro.time() < tempo:
+            motorB.run(-velT)
+            motorC.run(-velT)
+        motorC.hold()
+        motorB.hold()
+        while reflex2 > 60:
+            motorC.run(vel)
+        motorC.hold()
+        while reflex1 < 85:
+            motorB.run(-vel / 2)
+        motorB.hold()
+        while reflex2 < 85:
+            motorC.run(-vel / 2)
+
+    else:
+        cronometro.reset()
+        while cronometro.time() < tempo:
+            velT = vel - cronometro.time()
+            if velT < vel / 3:
+                velT = vel / 3
+            motorB.run(velT)
+            motorC.run(velT)
+        motorC.hold()
+        motorB.hold()
+        wait(200)
+        if reflex2 < 10:
+            ev3.speaker.beep()
+            flag = 1
+        wait(200)
+        if sensorc2.color() != None and sensorc2.color() != Color.WHITE:
+            ev3.speaker.beep(1000)
+            flag = 2
+        cronometro.reset()
+        while cronometro.time() < tempo:
+            motorB.run(-velT)
+            motorC.run(-velT)
+        motorC.hold()
+        motorB.hold()
+        while reflex1 > 60:
+            motorB.run(vel)
+        motorB.hold()
+        while reflex2 < 85:
+            motorC.run(-vel / 2)
+        motorC.hold()
+        while reflex1 < 85:
+            motorB.run(-vel / 2)
+
+    motorB.brake()
+    motorC.brake()
+    return flag
+
+
+def curva(angulo):  # angulo positivo: direita, negativo: esquerda
+    motorB.reset_angle(0)
+    motorC.reset_angle(0)
+    Kp = 3
+    Ki = 0.2
+    Kd = 8
+
+    t = 0
+    integ = 0
+    erro = 0
+    while True:
+        media = (motorB.angle() - motorC.angle()) / 2
+        erro0 = erro
+        erro = angulo - media
+
+        if erro == 0:
+            break
+        print(erro)
+
+        prop = erro * Kp
+        if -3 < erro < 3:
+            integ = integ + (erro * Ki)
+        t0 = t
+        t = cronometro.time()
+        tempoDecor = t - t0
+        if tempoDecor < 1:
+            tempoDecor = 1
+        deriv = ((erro - erro0) * Kd) / tempoDecor
+
+        correcao = prop + integ + deriv
+        vel = 5 + correcao
+        if vel < 0:
+            if vel > -5:
+                vel = -5
+        else:
+            if vel < 5:
+                vel = 5
+        motorC.dc(-vel)
+        motorB.dc(vel)
+
+    motorC.brake()
+    motorB.brake()
+
+
+def anda_reto_graus(
+    velBase, graus, stop
+):  # para dar ré os dois valores devem ser negativos
+    Kp = 3  # stop 1: hold 2: brake 3: coast
+    Ki = 0.02
+    Kd = 3
+
+    t = 0
+    integ = 0
+    erro = 0
+    media = 0
+    motorB.reset_angle(0)
+    motorC.reset_angle(0)
+    cronometro.reset()
+    if graus < 0:
+        while media > graus:
+            media = (motorB.angle() + motorC.angle()) / 2
+            erro0 = erro
+            erro = motorC.angle() - motorB.angle()
+
+            prop = erro * Kp
+            if -3 < erro < 3:
+                integ = integ + (erro * Ki)
+            t0 = t
+            t = cronometro.time()
+            tempoDecor = t - t0
+            if tempoDecor < 1:
+                tempoDecor = 1
+            deriv = ((erro - erro0) * Kd) / tempoDecor
+
+            correcao = prop + integ + deriv
+            motorC.run(velBase - correcao)
+            motorB.run(velBase + correcao)
+    else:
+        while media < graus:
+            media = (motorB.angle() + motorC.angle()) / 2
+            erro0 = erro
+            erro = motorC.angle() - motorB.angle()
+
+            prop = erro * Kp
+            if -3 < erro < 3:
+                integ = integ + (erro * Ki)
+            t0 = t
+            t = cronometro.time()
+            tempoDecor = t - t0
+            if tempoDecor < 1:
+                tempoDecor = 1
+            deriv = ((erro - erro0) * Kd) / tempoDecor
+
+            correcao = prop + integ + deriv
+            motorC.run(velBase - correcao)
+            motorB.run(velBase + correcao)
+    if stop == 1:
+        motorC.hold()
+        motorB.hold()
+    if stop == 2:
+        motorC.brake()
+        motorB.brake()
+    if stop == 3:
+        return
+
+
+distRodas = 15.2
+diamRodas = 5.7
+rotacao = ((distRodas) / diamRodas) * 360
+flag = 0
+# while flag!=1:
+#     flag = buraco(300)
+#     wait(100)
+#     anda_reto_graus(-300,-100,1)
+#     wait(200)
+curva(round(rotacao / 4))
+wait(100)
